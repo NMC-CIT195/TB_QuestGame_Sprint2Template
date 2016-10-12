7445038d595fd7738c64527641175e619ebd8f1c
John Velis <john@johnvelis.com>
2016-10-12 16:00:01 -0400

    initial commit

---
+++ TB_QuestGame/Models/TravelerAction.cs
@@ -0,0 +1,17 @@
+﻿namespace TB_QuestGame
+{
+    public enum TravelerAction
+    {
+        None,
+        MissionSetup,
+        LookAround,
+        Travel,
+        TravelerInfo,
+        TravelerInventory,
+        TravelerTreasure,
+        ListTARDISDestinations,
+        ListItems,
+        ListTreasures,
+        Exit
+    }
+}
